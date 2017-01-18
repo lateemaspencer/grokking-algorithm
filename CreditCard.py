ddcfd579bd13002a65d5db185e3b5abfe3505f5c
lateemaspencer <spencer.lateema@gmail.com>
2017-01-18 18:40:14 -0500

    created a credit card class to test classes

---
+++ CreditCard.py
@@ -0,0 +1,54 @@
+class CreditCard:
+    """A consumer credit card."""
+
+    def __init__(self, customer, bank, acnt, limit):
+        """Create a new credit card instance
+
+        The initial balance is zero.
+
+        customer the name of the customer (e.g. 'John Bowman')
+        bank     the name of the bank (e.g. 'California Savings')
+        acnt     the account identifier (e.g. '5391 0375 9387 5309')
+        limit    credit limit(measured in dollars)
+        """
+        self._customer = customer
+        self._bank = bank
+        self._account = acnt
+        self._limit = limit
+        self._balance = 0
+
+    def get_customer(self):
+        """Return name of the customer"""
+        return self._customer
+
+    def get_bank(self):
+        """Return the bank's name"""
+        return self._bank
+
+    def get_account(self):
+        """Return the card identifying number (typically stored as a string.)"""
+
+    def get_limit(self):
+        """Return current credit limit"""
+        return self._limit
+
+    def get_balance(self):
+        """Return current balance"""
+        return self._balance
+
+    def charge(self, price):
+        """Charge given price to the card, assuming sufficient credit limit
+
+        Return True if charge was processed; False if charge was denied.
+        """
+
+        if price + self._balance > self._limit:
+            return False
+        else:
+            self._balance += price
+            return True
+
+    def make_payment(self, amount):
+        "Process customer payment that reduces balance"
+
+        self._balance -= amount
